aa86c6f2e630d82945af0ac7e76e5d55d18d9faf
pashakeeper <pashagkjunost@gmail.com>
2025-11-06 19:08:01 +0000

    333

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -3,8 +3,13 @@ import avatar1 from "./avatar1.svg";
 import avatar2 from "./avatar2.svg";
 import avatar3 from "./avatar3.svg";
 import arrowRight from "./arrow_right.webp";
+import buttonImg from "./button_img.png";
+import play from "./play.png";
+import sound from "./sound.png";
+import icon_main from "./icon_main.webp";
+import dot from "./dot.png";
 
-export { logo, avatar1, avatar2, avatar3, arrowRight };
+export { logo, avatar1, avatar2, avatar3, arrowRight, buttonImg, play, sound, icon_main, dot };
 
 
 export const IMAGES = {
@@ -13,4 +18,9 @@ export const IMAGES = {
   avatar2,
   avatar3,
   arrowRight,
+  buttonImg,
+  play,
+  sound,
+  icon_main,
+  dot,
 } as const;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,20 +10,84 @@ export default function Home() {
       <Header />
       <section className="section_01" id="section_01">
         <div className="container-fluid">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-lg-9">
-              <h1>{t("hero.title_1")}<span className="dot">.</span> <br />
+              <h1>
+                {t("hero.title_1")}
+                <span className="dot">.</span> <br />
                 <span className="avatars_row">
                   <img src={IMAGES.avatar1} alt="Avatar 1" />
                   <img src={IMAGES.avatar2} alt="Avatar 2" />
                   <img src={IMAGES.avatar3} alt="Avatar 3" />
                   <i></i>
-                </span> {t("hero.title_2")} <span className="colored">{t("hero.title_3")}</span>
-                <span className="btn"><img src={IMAGES.arrowRight} alt="arrow right" /></span>
+                </span>{" "}
+                {t("hero.title_2")}{" "}
+                <span className="colored">{t("hero.title_3")}</span>
+                <span className="btn_img">
+                  <img src={IMAGES.buttonImg} alt="button img" />
+                </span>
               </h1>
             </div>
             <div className="col-lg-3">
               <p className="description">{t("hero.description")}</p>
+            </div>
+            <div className="line"></div>
+            <div className="col-lg-4 d-flex">
+              <a href="#" className="main_btn">
+                <span>
+                  {t("btn.btn_1")}
+                  <i><img src={IMAGES.dot} alt="" /></i>
+                </span>
+              </a>
+              <div className="controls_box">
+                <div className="play_pause">
+                  <img src={IMAGES.play} alt="Play" />
+                </div>
+                <div className="sound">
+                  <img src={IMAGES.sound} alt="Sound" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 data_box">
+              <div className="data_item">
+                <div className="data_quant">
+                  {t("description_with_data.data_1_quant")}
+                </div>
+                <div className="data_desc">
+                  {t("description_with_data.desc_1")}
+                </div>
+              </div>
+              <div className="data_item">
+                <div className="data_quant">
+                  {t("description_with_data.data_2_quant")}
+                </div>
+                <div className="data_desc">
+                  {t("description_with_data.desc_2")}
+                </div>
+              </div>
+              <div className="data_item">
+                <div className="data_quant">
+                  {t("description_with_data.data_3_quant")}
+                </div>
+                <div className="data_desc">
+                  {t("description_with_data.desc_3")}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section_02" id="section_02">
+        <div className="container-fluid grid_bg">
+          <div className="icon_box">
+            <img src={IMAGES.icon_main} alt="" />
+          </div>
+        </div>
+        <div className="container section_02_container">
+          <div className="row">
+            <div className="col-lg-12">
+              <a href="#" className="sec_btn">{t("btn.sec_btn_1")} <span>VS</span> {t("btn.sec_btn_1_1")}</a>
+              
             </div>
           </div>
         </div>
